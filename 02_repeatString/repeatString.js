c9c28fa4489f67069fa43d35f00ec4e379fe3634
Coder219 <gbdudenyc@gmail.com>
2024-08-20 13:56:42 -0400

    Did problems 1-7

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string,repeats) {
+    let output='';
+    if (repeats<0){
+        return'ERROR';
+    }
+    for (let num=0; num<repeats; num++){
+        output += string;
+    }
+    return output;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array, ...args) {
+   for(const arg of args){
+        while (array.includes(arg)){
+            index = array.indexOf(arg);
+            array.splice(index,1);
+        }
+    }  
+    return array;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
