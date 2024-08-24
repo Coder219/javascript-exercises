3ede46a7a2c6ba9c1a55fe8f524660bac0148317
Coder219 <145808304+Coder219@users.noreply.github.com>
2024-08-23 21:18:15 -0400

    made 02 cleaner

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,11 +1,7 @@
 const repeatString = function(string,repeats) {
     let output='';
-    if (repeats<0){
-        return'ERROR';
-    }
-    for (let num=0; num<repeats; num++){
-        output += string;
-    }
+    if (repeats<0) return'ERROR';
+    for (let num=0; num<repeats; num++) output += string;
     return output;
 };
 
